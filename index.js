ea8140f1287b2c4709b76a184920e4c788748b01
simzam <siyanda.mzam@hellocomputer.com>
2017-09-15 12:56:17 +0200

    Big fat commit

---
+++ index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.get('/about', function(request, response) {
+  response.render('pages/about');
+});
+
+app.get('/codes', function(request, response) {
+  response.render('pages/codes');
+});
+
+app.get('/contact', function(request, response) {
+  response.render('pages/contact');
+});
+
+app.get('/gallery', function(request, response) {
+  response.render('pages/gallery');
+});
+
+app.get('/icons', function(request, response) {
+  response.render('pages/icons');
+});
+
+app.get('/single.html', function(request, response) {
+  response.render('pages/single');
+});
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
